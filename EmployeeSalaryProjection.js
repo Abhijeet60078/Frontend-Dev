b3fbb31726ff5a91f86e33cfe683489affaebb4c
Amishra078 <mishraabhijeet078@gmail.com>
2025-11-18 23:00:02 +0530

    javascript folder initial commit

---
+++ EmployeeSalaryProjection.js
@@ -0,0 +1,14 @@
+let currentSalary = 30000;
+let incrementRate = 10; // %
+
+let salaryTable = [];
+
+for (let year = 1; year <= 5; year++) {
+    currentSalary += currentSalary * (incrementRate / 100);
+    salaryTable.push({
+        Year: year,
+        Salary: Math.round(currentSalary)
+    });
+}
+
+console.table(salaryTable);
---
+++ PersonalizedLogin.js
@@ -0,0 +1,15 @@
+let userName = "Pragati";
+let currentHour = new Date().getHours(); // returns 0–23
+
+// Logic for greeting
+let greeting = "";
+
+if (currentHour < 12) {
+    greeting = `Good Morning ${userName}!`;
+} else if (currentHour >= 12 && currentHour < 17) {
+    greeting = `Good Afternoon ${userName}!`;
+} else {
+    greeting = `Good Evening ${userName}!`;
+}
+
+console.log(greeting);
