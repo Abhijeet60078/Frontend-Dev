925af1cc6888f4ee18e081e6582df28e84101a25
Amishra078 <mishraabhijeet078@gmail.com>
2025-11-20 13:06:30 +0530

    Prototype in ES6 Version 2  is committed by Abhijeet

---
+++ Prototype.js
@@ -0,0 +1,20 @@
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+    showName() {
+        console.log(this.name);
+    }
+}
+class Student extends Person {
+    constructor(name, branch) {
+        super(name);
+        this.branch = branch;
+    }
+    showBranch() {
+        console.log(this.branch);
+    }
+}
+const st = new Student("Abhijeet", "CSE");
+st.showName();
+st.showBranch();
